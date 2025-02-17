6b263d884a476a795236c5d09272678fde7652a4
mushi1155 <musharafaif155@gmail.com>
2025-02-17 18:06:46 +0500

    jack game

---
+++ index.js
@@ -0,0 +1,39 @@
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard,secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEL= document.getElementById("message-el")
+let sumEL= document.getElementById("sum-el")
+let cardsEL= document.getElementById("cards-el")
+
+
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+function startGame(){
+    renderGame()
+}
+function renderGame(){
+    cardsEL.textContent= "Cards: " + cards[0] +"," + cards[1]
+    sumEL.textContent= "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! "
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false
+    }
+    messageEL.textContent=message
+
+}
+function newCard(){
+    console.log("Drawing a new card from the deck")
+    let card = 7
+    sum += card
+    renderGame()
+}
+
